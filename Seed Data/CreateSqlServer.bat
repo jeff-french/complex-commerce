c13f908cb4102de536ce1b299579309f100bbf41
Jeff French <jeff.french@moonswitch.com>
2013-03-06 20:35:55 -0600

    Update SqlServer seed scripts.

--- Seed Data/CreateSqlServer.bat
+++ Seed Data/CreateSqlServer.bat
@@ -17,18 +17,16 @@ goto END
 echo Options:
 echo.
 echo 1. Run Chinook_SqlServer.sql
-echo 2. Run Chinook_SqlServer_AutoIncrementPKs.sql
 echo 3. Exit
 echo.
 choice /c 123
 if (%ERRORLEVEL%)==(1) set SQLFILE=Chinook_SqlServer.sql
-if (%ERRORLEVEL%)==(2) set SQLFILE=Chinook_SqlServer_AutoIncrementPKs.sql
 if (%ERRORLEVEL%)==(3) goto END
 
 :RUNSQL
 echo.
 echo Running %SQLFILE%...
-sqlcmd -E -S .\sqlexpress -i %SQLFILE% -b -m 1
+sqlcmd -E -S (local) -i %SQLFILE% -b -m 1
 
 :END
 echo.
